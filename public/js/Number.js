4049300c93415eca51a40cf782b88e6e51a8c14c
Sedorikku1949 <cedic2005@gmail.com>
2022-03-16 08:10:06 +0100

    remove public

--- public/js/Number.js
+++
@@ -1,5 +0,0 @@
-function shortNumber(){
-  const tab = ['y', 'z', 'e', 'p', 't', 'g', 'm', 'k']
-  for (let i = 24, y = 0; i > 0; i -= 3, y++) if (this >= 10 ** i) return (this / 10 ** i).toFixed((this / 10 ** i).toFixed(1).toString().includes('.0') ? 0 : 1) + tab[y].toUpperCase()
-  return this
-}
